3f191ff663c5c3344a9415d3315443890a0a7446
cadeboden912 <70959951+cadeboden912@users.noreply.github.com>
2025-07-19 12:15:05 -0700

    Code Edits and Collison Edits

--- components/plug.gd
+++ components/plug.gd
@@ -4,7 +4,7 @@ extends StaticBody2D
 @onready var sprite = $Sprite2D
 
 func _ready():
-	position = Globals.snap_to_center(position)
+	
 	interactable.interact = _on_interact
 
 func _on_interact():
